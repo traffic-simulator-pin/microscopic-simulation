878f21091473b8fd2674ded4e6c496ccf6ca3c84
jefip <jefip@NoteDoJefi>
2019-05-06 19:20:11 -0300

    Ajustar problemas no log e execução.

--- src/br/udesc/ceavi/pin2/utils/GeradorRede.java
+++ src/br/udesc/ceavi/pin2/utils/GeradorRede.java
@@ -41,7 +41,7 @@ public class GeradorRede extends ExecucaoMultiEtapas{
     private void criaRedeTrafego() {
         SimulacaoMicroscopica.getInstance().log("Iniciando geração da rede de tráfego.");
         Thread terminal = SimulacaoMicroscopica.getInstance().getShellCommand().getNewShell(this,
-             "netconvert --osm-files " + this.arquivoSimulacao.getAbsolutePath() + " --geometry.remove --roundabouts.guess --ramps.guess --junctions.join --tls.guess-signals --tls.discard-simple --tls.join -o " + SimulacaoMicroscopica.getInstance().getWorkspaceFolder() + "/rede.net.xml"
+             "netconvert --osm-files " + this.arquivoSimulacao.getAbsolutePath() + " --geometry.remove --roundabouts.guess --ramps.guess --junctions.join --tls.guess-signals --tls.discard-simple --tls.join -o " + SimulacaoMicroscopica.getInstance().getWorkspaceFolder() + File.separator + "rede.net.xml"
         );
         terminal.start();
     }
@@ -53,8 +53,16 @@ public class GeradorRede extends ExecucaoMultiEtapas{
         SimulacaoMicroscopica.getInstance().log("Iniciando geração de arquivo de pontos de interesse.");
         File polygonUtil = new File("src/br/udesc/ceavi/pin2/utils/osmPolyconvert.typ.xml");
         Thread terminal = SimulacaoMicroscopica.getInstance().getShellCommand().getNewShell(this,
-             "polyconvert --net-file " + this.arquivoSimulacao.getAbsolutePath() + " --osm-files " + this.arquivoSimulacao.getAbsolutePath() + " --type-file " + polygonUtil.getAbsolutePath() + " -o " + SimulacaoMicroscopica.getInstance().getWorkspaceFolder() + "/poi.net.xml"
+             "polyconvert --net-file " + SimulacaoMicroscopica.getInstance().getWorkspaceFolder() + File.separator + "rede.net.xml --osm-files " + this.arquivoSimulacao.getAbsolutePath() + " --type-file " + polygonUtil.getAbsolutePath() + " -o " + SimulacaoMicroscopica.getInstance().getWorkspaceFolder() + File.separator + "poi.net.xml"
         );
         terminal.start();
+    }
+    
+    @Override
+    /**
+     * {@inheritdoc}
+     */
+    protected int getTotalEtapas(){
+        return 2;
     }
 }
